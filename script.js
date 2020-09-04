26dad27cc491076001e69b3725d85e04ca3caea5
Developer Raj <rajdeepkakar@gmail.com>
2020-09-03 22:02:32 -0400

    Removed commented text from the javascript file. The commented text at very end of the file did not provide any value.

--- script.js
+++ script.js
@@ -129,13 +129,3 @@ function writePassword() {
 
 //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//Ask the user a series of questions
-// a) length of password (numeric input)
-// b) validate length is between 8 and 128 characters
-// c) type of characters to include - special characters, uppercase, lowercase, numeric (series of yes/no confirms)
-// d) validate the user selected at least one character type
-
-//Any invalid response will terminate password generation and the user must start over.
-
-//Once th user entered valid responses, generate a password that meets their criteria and display it on the box.
